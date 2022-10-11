7e3c26b69b7fe9c82688daffd4571cf2d39ac1ba
Mathis Burger <mburger@die-netzwerkstatt.de>
2022-10-11 16:16:14 +0200

    [TASK] Updated index web template

--- src/controller/template_controller.rs
+++ src/controller/template_controller.rs
@@ -5,7 +5,6 @@ use crate::AppState;
 #[get("/")]
 pub(crate) async fn index_page(data: web::Data<AppState>) -> HttpResponse {
     let mut ctx = Context::new();
-    ctx.insert("name", "Mathis");
     let rendered = data.tmpl.render("index.html", &ctx).unwrap();
     HttpResponse::Ok().body(rendered)
 }
